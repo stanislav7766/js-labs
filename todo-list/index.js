1e5c1739d15b781b8f409864693cecd78f23b0de
stanislav7766 <stanislavtihonov5@gmail.com>
2020-06-10 16:58:02 +0300

    added more  explanation code for todo-list

--- todo-list/index.js
+++ todo-list/index.js
@@ -1,32 +1,33 @@
-import {
-  getTasks,
-  setTasks,
-  clearUL,
-  emptyStr,
-  sortTasks,
-} from './modules/helpers.js';
-
+import { getTasks, setTasks, clearUL, emptyStr, sortTasks } from './modules/helpers.js';
+// часть 2. обновление списка дел на станичке браузера, на вход у нас список дел с localStorage
 const updateList = (tasks) => {
-  const ul = clearUL();
+  // часть связи узлов такая:  ... ->ul->li
+  //                                   ->li
+  //                                   ->li
+  //                                    ...
+  const ul = clearUL(); //зачистили предыдущий список
   for (let i = 0; i < tasks.length; i++) {
+    //циклом проходимся по новому полученому списку,на каждом элементе со списка создаем новый узел, заполняем текстом и приклеиваем его к ul, для связей дерева
     const li = document.createElement('li');
     li.appendChild(document.createTextNode(tasks[i]));
     ul.appendChild(li);
   }
 };
-
+//часть 3, btnListener вызывается когда пользователь нажимает на кнопку "Добавить", тут все просто , но лучше пройдемся построчно
 const btnListener = () => {
-  const input = document.getElementById('input');
-  const task = input.value;
+  const input = document.getElementById('input'); //нашли поле ввода по id
+  const task = input.value; //получили введеный текст с поля ввода
   if (emptyStr(task)) {
+    //проверка пустой текст или нет, если пусто , то в список ничего не добавится и пользователю покажется сообщение "Добавьте задачу"
     alert('Добавьте задачу');
     return;
   }
-  const tasks = getTasks();
-  const sortedTasks = sortTasks([...tasks, task]);
-  setTasks(sortedTasks);
-  updateList(sortedTasks);
-  input.value = '';
+  // если же текст не путой то идем дальше
+  const tasks = getTasks(); //считаем те задачи, что уже есть в хранилище
+  const sortedTasks = sortTasks([...tasks, task]); // отсортируем предыдущие задачи уже с новой
+  setTasks(sortedTasks); //обновим все задачи в хранилище
+  updateList(sortedTasks); //и обновим их на страничке (часть2)
+  input.value = ''; //так же затрем текст поля ввода, чтобы можно было добавлять новое дело
 };
 
 const initList = (container) => {
@@ -54,14 +55,15 @@ const initBtn = (container) => {
   container.appendChild(btn);
   btn.onclick = btnListener;
 };
-
+// часть 1. как открывается\перезагружается страничка мы попадаем в код ниже. суть лабораторки - показать умения работать с localStorage(нужно почитать https://developer.mozilla.org/ru/docs/Web/API/Window/localStorage) это как база данных,хранилище, только оно находится в браузере, а не где то удаленно, то есть есть ли мы добавили что-то в список и перезагрузили страничку или через время снова зашли на страничку, то дело так и останется на страничке, не нужно будет его снова добавлять
+// мноние вещи схожи с предыдущими лабораторками, каждую строчку пояснять не буду
 (() => {
   const container = document.getElementById('container');
   initList(container);
   initInput(container);
   initBtn(container);
-  const tasks = getTasks();
-  updateList(tasks);
+  const tasks = getTasks(); //тут мы проверяем в нашем localStorage есть ли записи
+  updateList(tasks); //и обновляем их на страничке(часть 2)
   window.onunload = () => {
     document.getElementById('input').removeEventListener('keyup', () => {});
     return;
--- todo-list/modules/helpers.js
+++ todo-list/modules/helpers.js
@@ -1,17 +1,19 @@
+// функция для проверки пустая строка или нет
 export const emptyStr = (value) => value === '';
-
+// функция для поиска узла по id, зачистке его текста(того что отображается на страничке)
 export const clearUL = () => {
   const ul = document.getElementById('ul');
   ul.innerHTML = '';
   return ul;
 };
+// функция для чтения записей с localStorage (записи в виде одной строчки записаны) и если есть записи,то функция  возвращает расспарсенный(из строки преобразует структуру данных) массив этих записей или же возвращает пустой массив если записей нету
 export const getTasks = () => {
   const taskJson = localStorage.getItem('tasks') || '[]';
   return JSON.parse(taskJson);
 };
-
-export const setTasks = (tasks) =>
-  localStorage.setItem('tasks', JSON.stringify(tasks));
+// функция делает запись в localStorage, принимает на вход массив записей , преобразует их в строку и записывает в localStorage
+export const setTasks = (tasks) => localStorage.setItem('tasks', JSON.stringify(tasks));
+// функция для сортировки записей со списка дел, в задании было написано, что это необходимо
 export const sortTasks = (tasks) =>
   tasks.sort((a, b) => {
     const aa = a.toLowerCase(),
